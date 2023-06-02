72f21a33b450b1318203aa93eb8f552892434b80
GoktugYmsk <gogtug033@outlook.com>
2023-06-02 11:43:56 +0300

    rightbar split into components

--- src/components/rightBar/index.jsx
+++ src/components/rightBar/index.jsx
@@ -1,27 +1,18 @@
 import React, { useState } from 'react';
-import { HiOutlineTruck } from 'react-icons/hi';
-import { useDispatch } from 'react-redux';
+
 import data from '../../assets/data';
-import { setPicture } from '../../configure/configure';
+
+import RightAltBar from './rightAltBar';
+import RightTopBar from './rightTopBar';
 import './index.scss';
 
 const RightBar = () => {
-    const [selectedColor, setSelectedColor] = useState('');
-    const [selectedSize, setSelectedSize] = useState('');
-    const star = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa0HmahLflc6QVFR2q2URFte5EJjChKmGCNA&usqp=CAU'
+        
     const [amountError, setAmounError] = useState(false)
     const [inputValue, setInputValue] = useState()
     const [activeBarem, setActiveBarem] = useState(false)
 
     const [list, setList] = useState(data);
-
-    const dispatch = useDispatch()
-
-    const handleColorChange = (color) => {
-        setSelectedColor(color);
-        setSelectedSize('');
-        dispatch(setPicture(color))
-    };
 
     const handleSizeChange = (size) => {
         setSelectedSize(size);
@@ -35,106 +26,28 @@ const RightBar = () => {
         );
     };
 
-    const isSizeActive = (size) => {
-        const variants = getVariantByColor(selectedColor);
-        return variants.some(
-            (variant) => variant.attributes.find((attribute) => attribute.name === 'Beden').value === size
-        );
-    };
-
     const handleChange = (e) => {
         setInputValue(e.target.value);
 
         const baremAmountMax = Math.max(...list.baremList.map((barem) => barem.maximumQuantity));
         const baremAmountMin = Math.min(...list.baremList.map((barem) => barem.minimumQuantity));
 
-        if (baremAmountMin < e.target.value && e.target.value < baremAmountMax) {
-            setActiveBarem(true);
-        } else if (e.target.value > 500) {
+        if (e.target.value > 500) {
             setAmounError(true);
+        }
+        else if (baremAmountMin < e.target.value && e.target.value < baremAmountMax) {
+            setActiveBarem(true);
         } else {
             setActiveBarem(false);
             setAmounError(false);
         }
     };
 
-
     return (
         <div className="container-RightBar">
-            <h3>{data.productTitle}</h3>
-            <div className='container__icons'>
-                <img src={star} alt="Star" />
-                <img src={star} alt="Star" />
-                <img src={star} alt="Star" />
-                <img src={star} alt="Star" />
-                <img src={star} alt="Star" />
-                <p>30 Yorum</p>
-            </div>
-            <div className='container__rightBar-piece' >
-                <h1>600 Tl - 800 Tl <span className='container__rightBar__span' >/adet</span></h1>
-                <span className='container__rightBar-piece__span' >100 adet ( Minimum sipariş adedi)</span>
-            </div>
-            <div className='rightbar__content'>
-                <div className='rightbar__content__top-info'>
-                    {data.selectableAttributes
-                        .filter((attribute) => attribute.name === 'Renk')
-                        .map((attribute) => (
-                            <div key={attribute.name}>
-                                <h4>{attribute.name}</h4>
-                                <div className='rightbar__content__title'>
-                                    {attribute.values.map((value) => (
-                                        <p key={value} onClick={() => handleColorChange(value)} className={selectedColor === value ? 'active' : ''}>
-                                            {value}
-                                        </p>
-                                    ))}
-                                </div>
-                            </div>
-                        ))}
-                </div>
-                <div className='rightbar__alt__content'>
-                    <h4>Beden</h4>
-                    <div className='rightbar__content-altinformation'>
-                        {selectedColor &&
-                            data.selectableAttributes
-                                .find((attribute) => attribute.name === 'Beden')
-                                .values.map((size) => (
-                                    <button key={size} onClick={() => handleSizeChange(size)} className={isSizeActive(size) ? 'active' : 'inactive'} disabled={!isSizeActive(size)}>
-                                        {size}
-                                    </button>
-                                ))}
-                    </div>
-                </div>
-            </div>
-            <div className='container__barem' >
-                <div className='container__barem__tittle'>
-                    <h2>Toptan Fiyat</h2>
-                    <p>(Adet)</p>
-                </div>
-                <div className='container__barem-bar' >
-                    {list.baremList.map((barem, keyBarem) => (
-                        <div className='barem' key={keyBarem}>
-                            <p className={`barem__p ${activeBarem ? 'baremClassActive' : ''}`} >{barem.minimumQuantity}</p>
-                            <p className={`barem__p ${activeBarem ? 'baremClassActive' : ''}`}   >{barem.maximumQuantity}</p>
-                        </div>
-                    ))}
-                </div>
-                <div className='barem__amount__info' >
-                    <p>Adet</p>
-                    <input onChange={handleChange} placeholder="100" />
-                    <p className='barem__amount__piece' >Adet</p>
-                    <p className={`barem__amount-p ${amountError ? 'errorColor' : ''}`} >Stok Adeti: 500</p>
-                </div>
-            </div>
-            <div className='barem__altInfo' >
-                <h3>Toplam:</h3>
-                <div className='barem__altInfo__content' >
-                    <p className='barem__first-chlid'><strong>3000 Tl</strong></p>
-                    <p className='barem__second-child' >Kargo Ücreti :</p>
-                    <p className='barem__second-third' ><span>Alıcı Öder</span></p>
-                    <p className='barem__price__option' ><span>Ödeme Seçenekleri</span></p>
-                </div>
-                <button className='barem__button' >SEPETE EKLE</button>
-            </div>
+            <RightTopBar handleSizeChange={handleSizeChange} getVariantByColor={getVariantByColor}
+                setSelectedSize={setSelectedSize} />
+            <RightAltBar list={list} activeBarem={activeBarem} handleChange={handleChange} amountError={amountError} />
         </div >
     );
 };

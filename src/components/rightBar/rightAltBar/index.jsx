3f49432590d20431193ed68b167aabdff872b96f
GoktugYmsk <gogtug033@outlook.com>
2023-06-03 11:35:49 +0300

    the default properties of the page are set

--- src/components/rightBar/rightAltBar/index.jsx
+++ src/components/rightBar/rightAltBar/index.jsx
@@ -1,50 +1,40 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import React from 'react'
 
-
-function RightAltBar({ list, activeBarem, handleChange, amountError }) {
+function RightAltBar({list,activeBarem,handleChange,amountError}) {
     return (
         <div>
-            <Row className='container__barem'>
-                <Col lg={12} md={12} sm={12}>
-                    <div className='container__barem__tittle'>
-                        <h2>Toptan Fiyat</h2>
-                        <p>(Adet)</p>
-                    </div>
-                </Col>
-                <Col lg={12} md={12} sm={12}>
-                    <div className='container__barem-bar'>
-                        {list.baremList.map((barem, keyBarem) => (
-                            <div className='barem' key={keyBarem}>
-                                <p className={`barem__p ${activeBarem ? 'baremClassActive' : ''}`}>{barem.minimumQuantity}</p>
-                                <p className='barem__p'>{barem.maximumQuantity}</p>
-                            </div>
-                        ))}
-                    </div>
-                </Col>
-                <Col lg={12} md={12} sm={12}>
-                    <div className='barem__amount__info'>
-                        <p>Adet</p>
-                        <input onChange={handleChange} placeholder="100" />
-                        <p className='barem__amount__piece'>Adet</p>
-                        <p className={`barem__amount-p ${amountError ? 'errorColor' : ''}`}>Stok Adeti: 500</p>
-                    </div>
-                </Col>
-            </Row>
-            <Row className='barem__altInfo'>
-                <Col lg={12} md={12} sm={12}>
-                    <h3>Toplam:</h3>
-                    <div className='barem__altInfo__content'>
-                        <div className='barem__first-chlid'><strong>3000 Tl</strong></div>
-                        <div className='barem__second-child'>Kargo Ücreti :</div>
-                        <div className='barem__second-third'><span>Alıcı Öder</span></div>
-                        <div className='barem__price__option'><span>Ödeme Seçenekleri</span></div>
-                    </div>
-                    <button className='barem__button'>SEPETE EKLE</button>
-                </Col>
-            </Row>
+            <div className='container__barem' >
+                <div className='container__barem__tittle'>
+                    <h2>Toptan Fiyat</h2>
+                    <p>(Adet)</p>
+                </div>
+                <div className='container__barem-bar' >
+                    {list.baremList.map((barem, keyBarem) => (
+                        <div className='barem' key={keyBarem}>
+                            <p className={`barem__p ${activeBarem ? 'baremClassActive' : ''}`} >{barem.minimumQuantity}</p>
+                            <p className='barem__p' >{barem.maximumQuantity}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className='barem__amount__info' >
+                    <p>Adet</p>
+                    <input onChange={handleChange} placeholder="100" />
+                    <p className='barem__amount__piece' >Adet</p>
+                    <p className={`barem__amount-p ${amountError ? 'errorColor' : ''}`} >Stok Adeti: 500</p>
+                </div>
+            </div>
+            <div className='barem__altInfo' >
+                <h3>Toplam:</h3>
+                <div className='barem__altInfo__content' >
+                    <p className='barem__first-chlid'><strong>3000 Tl</strong></p>
+                    <p className='barem__second-child' >Kargo Ücreti :</p>
+                    <p className='barem__second-third' ><span>Alıcı Öder</span></p>
+                    <p className='barem__price__option' ><span>Ödeme Seçenekleri</span></p>
+                </div>
+                <button className='barem__button' >SEPETE EKLE</button>
+            </div>
         </div>
     )
 }
 
-export default RightAltBar;
+export default RightAltBar
